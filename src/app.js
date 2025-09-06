e64fd2c84b5b2d7044dfa49d63262ab7b8b6d274
Mykhailo Forkosh <forkos277@gmail.com>
2025-09-06 20:27:46 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-// write code here
+/* eslint-disable no-console */
+'use strict';
+
+const fsCb = require('fs');
+const path = require('path');
+
+const [subject, destination] = process.argv.slice(2);
+
+let fileName;
+let destinationIsDirectory = false;
+
+try {
+  fileName = path.basename(subject);
+  destinationIsDirectory = fsCb.statSync(destination).isDirectory();
+} catch (error) {}
+
+const correctDestination = destinationIsDirectory
+  ? path.join(destination, fileName)
+  : destination;
+
+try {
+  fsCb.renameSync(subject, correctDestination);
+} catch (err) {
+  console.error(err);
+}
